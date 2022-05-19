d7eb7a1a24de0afc3def7d56bee9853b168d731d
Omen1984 <ivan.plehanov42@yandex.ru>
2022-05-19 09:57:31 +0500

    version 1.0

---
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -0,0 +1,48 @@
+package ru.netology.javacore;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TodoServer {
+    private int port;
+    private Todos todos;
+
+    public TodoServer(int port, Todos todos) {
+        this.port = port;
+        this.todos = todos;
+    }
+
+    public void start() throws IOException {
+        System.out.println("Starting server at " + port + "...");
+        ServerSocket serverSocket = new ServerSocket(port);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            final String inputJsonTask = in.readLine();
+            final JsonObject root = JsonParser.parseString(inputJsonTask).getAsJsonObject();
+
+            String type = root.get("type").toString().replaceAll("\"", "");
+            String task = root.get("task").toString().replaceAll("\"", "");
+
+            switch (type) {
+                case "ADD":
+                    todos.addTask(task);
+                    break;
+                case "REMOVE":
+                    todos.removeTask(task);
+                    break;
+            }
+            out.println("Задачи: " + todos.getAllTasks());
+        }
+    }
+}
